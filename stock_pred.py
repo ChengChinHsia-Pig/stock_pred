2420dfde892d0d9c681647be1325d6fd78efb797
ChengChinHsia <103416952+ChengChinHsia-Pig@users.noreply.github.com>
2023-10-01 17:48:28 +0800

    Update stock_pred.py

--- stock_pred.py
+++ stock_pred.py
@@ -6,47 +6,38 @@ from datetime import datetime, timedelta
 import requests
 import os
 import wget
+import time
+import concurrent.futures
 
-# 读取CSV文件
-comp = "AMD"
-url = f"https://query1.finance.yahoo.com/v7/finance/download/{comp}?period1=916963200&period2=1694304000&interval=1d&events=history&includeAdjustedClose=true"
+unix = round(time.time())
+comp = "AAPL"
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{comp}?period1=000000000&period2={unix}&interval=1d&events=history&includeAdjustedClose=true"
 wget.download(url, "temp.csv")
 print(f"Downloaded {comp} Stock Data.")
 
-# 读取数据并删除包含NaN的行
-data = pd.read_csv('temp.csv')  # 将文件名替换为你的CSV文件名
+start_time = time.time()
+data = pd.read_csv('temp.csv')
 data['Date'] = pd.to_datetime(data['Date'])
 data.set_index('Date', inplace=True)
-data.dropna(axis=0, inplace=True)  # 删除包含NaN的行
+data.dropna(axis=0, inplace=True)
 
-# 仅保留2020年到2024年的数据
 start_date = datetime(1950, 1, 1)
 end_date = datetime(2024, 12, 31)
 data = data[(data.index >= start_date) & (data.index <= end_date)]
 
-# 计算总行数并根据总行数生成 look_back_days_values
 total_rows = len(data)
 look_back_step = 1000
 look_back_max = total_rows // 1000 * 1000
-
 look_back_days_values = list(range(1000, look_back_max + 1, look_back_step))
-
-# 预测未来一段时间的走势并绘制在同一个窗口中
-forecast_days = 365*2
+forecast_days = 365
 forecast_dates = [data.index[-1] + timedelta(days=i) for i in range(1, forecast_days + 1)]
 
 num_rows = 3
 num_cols = 4
 num_subplots = num_rows * num_cols
-
-# 创建第一个视窗
 fig1, axes1 = plt.subplots(num_rows, num_cols, figsize=(18, 14))
 fig1.tight_layout()
-
-for idx, look_back_days in enumerate(look_back_days_values):
-    row = idx // num_cols
-    col = idx % num_cols
-
+def train_and_predict(look_back_days):
     X = []
 
     for i in range(len(data) - look_back_days):
@@ -55,7 +46,6 @@ for idx, look_back_days in enumerate(look_back_days_values):
     X = np.array(X)
     y = data['Close'][look_back_days:].values
 
-    # 训练线性回归模型
     model = LinearRegression()
     model.fit(X, y)
 
@@ -68,7 +58,15 @@ for idx, look_back_days in enumerate(look_back_days_values):
         last_data = np.roll(last_data, -1)
         last_data[0, -1] = predicted_price
 
-    # 在子图中绘制预测结果的路径
+    return look_back_days, predicted_prices
+
+num_threads = 64
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+    results = list(executor.map(train_and_predict, look_back_days_values))
+
+for idx, (look_back_days, predicted_prices) in enumerate(results):
+    row = idx // num_cols
+    col = idx % num_cols
     ax = axes1[row, col]
     ax.plot(data.index, data['Close'], label='Close')
     ax.plot(forecast_dates, predicted_prices, label=f'Look Back: {look_back_days}', linestyle='dashed')
@@ -78,24 +76,23 @@ for idx, look_back_days in enumerate(look_back_days_values):
     ax.legend()
 
 plt.subplots_adjust(hspace=0.5)
-
-# 创建第二个视窗
 fig2, ax2 = plt.subplots(figsize=(10, 6))
-
-# 计算平均值并绘制
 average_predicted_prices = np.mean(np.array([ax.lines[line_idx * 2 + 1].get_ydata() for line_idx in range(len(look_back_days_values)) if len(ax.lines) > line_idx * 2 + 1]), axis=0)
-
-# 绘制前一年的数据（蓝色实线）
 one_year_ago = data.index[-forecast_days:]
 one_year_ago_prices = data['Close'][-forecast_days:]
 
-ax2.plot(one_year_ago, one_year_ago_prices, label='One Year Ago', linestyle='solid')
-ax2.plot(forecast_dates, average_predicted_prices, label='Average Predicted Price', linestyle='dashed')
-ax2.set_xlabel('Date')
-ax2.set_ylabel('Price(USD)')
-ax2.set_title(f'Average {comp} Stock Prediction')
-ax2.legend()
+try:
+    ax2.plot(one_year_ago, one_year_ago_prices, label=f'{forecast_days} Days Ago', linestyle='solid')
+    ax2.plot(forecast_dates, average_predicted_prices, label='Average Predicted Price', linestyle='dashed')
+    ax2.set_xlabel('Date')
+    ax2.set_ylabel('Price(USD)')
+    ax2.set_title(f'Average {comp} Stock Prediction')
+    ax2.legend()
+except ValueError as e:
+    print("資料錯誤:", e)
+    os.remove("temp.csv")
 
-# 显示第二个视窗
+total_time = time.time()-start_time
+print(f"Total time: {total_time} s")
 plt.show()
 os.remove("temp.csv")
